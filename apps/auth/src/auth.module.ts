f3553333e3034ae29f0f7b22544bbfd1554a1610
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2024-02-04 22:03:18 +0700

    Section 6 - Chapter 32 ~ Health Check

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { LoggerModule } from '@app/common';
+import { HealthModule, LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -32,6 +32,7 @@ import * as Joi from 'joi';
       }),
       inject: [ConfigService],
     }),
+    HealthModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
